0f071df7c1bc82b35a4f77224da16dcce5701b78
Hector Virgen <hvirgen@tagged.com>
2013-12-01 12:47:35 -0800

    Build production files

--- build/taggedInfiniteScroll.js
+++ build/taggedInfiniteScroll.js
@@ -425,54 +425,46 @@ define('src/taggedInfiniteScroll',['angular', 'underscore'], function(angular, _
   module.directive('taggedInfiniteScroll', ['$window', '$timeout', function($window, $timeout) {
     var SCROLL_THROTTLE = 50;
 
-    return function(scope, elem, attrs) {
-      var $win = angular.element($window);
-      var enabled = true;
-      var scrollDistance = 0;
+    return {
+      scope: {
+        callback: '&taggedInfiniteScroll',
+        distance: '=taggedInfiniteScrollDistance',
+        disabled: '=taggedInfiniteScrollDisabled'
+      },
+      link: function(scope, elem, attrs) {
+        var $win = angular.element($window);
 
-      var onScroll = _.throttle(function() {
-        if (!enabled) return;
+        var onScroll = _.throttle(function() {
+          // Do nothing if infinite scroll has been disabled
+          if (scope.disabled) {
+            return;
+          }
 
-        var windowHeight = $win.height();
-        var elementBottom = elem.offset().top + elem.height();
-        var windowBottom = windowHeight + $win.scrollTop();
-        var remaining = elementBottom - windowBottom;
-        var shouldGetMore = (remaining - scrollDistance <= 0);
+          var windowHeight = $win.height();
+          var elementBottom = elem.offset().top + elem.height();
+          var windowBottom = windowHeight + $win.scrollTop();
+          var remaining = elementBottom - windowBottom;
+          var shouldGetMore = (remaining - parseInt(scope.distance || 0, 10) <= 0);
 
-        if (shouldGetMore) getMore();
-      }, SCROLL_THROTTLE);
+          if (shouldGetMore) {
+            $timeout(scope.callback);
+          }
+        }, SCROLL_THROTTLE);
 
-      var getMore = function() {
-        $timeout(function() {
-          scope.$eval(attrs.taggedInfiniteScroll);
+        // Check immediately if we need more items upon reenabling.
+        scope.$watch('disabled', onScroll);
+
+        $win.bind('scroll', onScroll);
+
+        // Remove window scroll handler when this element is removed.
+        scope.$on('$destroy', function() {
+          $win.unbind('scroll', onScroll);
         });
-      };
 
-      if (attrs.taggedInfiniteScrollDistance) {
-        scope.$watch(attrs.taggedInfiniteScrollDistance, function(value) {
-          scrollDistance = parseInt(value, 10);
-        });
+        // Check on next event loop to give the browser a moment to paint.
+        // Otherwise, the calculations may be off.
+        $timeout(onScroll);
       }
-
-      if (attrs.taggedInfiniteScrollDisabled) {
-        scope.$watch(attrs.taggedInfiniteScrollDisabled, function(value) {
-          enabled = !value;
-
-          // Check immediately if we need more items upon reenabling.
-          onScroll();
-        });
-      }
-
-      $win.bind('scroll', onScroll);
-
-      // Remove window scroll handler when this element is removed.
-      scope.$on('$destroy', function() {
-        $win.unbind('scroll', onScroll);
-      });
-
-      // Check on next event loop to give the browser a moment to paint.
-      // Otherwise, the calculations may be off.
-      $timeout(onScroll);
     };
   }]);
 });
